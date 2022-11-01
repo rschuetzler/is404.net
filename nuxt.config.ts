80eef4c4d53cfa84dcb2b63ac9e7f69b6ad0cb7e
Ryan Schuetzler <ryan@schuetzler.net>
2022-11-01 16:17:51 -0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss'], //https://tailwindcss.nuxtjs.org/getting-started/setup
+    srcDir: 'src/'
+})
